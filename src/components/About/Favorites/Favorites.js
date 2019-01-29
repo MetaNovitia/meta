cdfe9c0e8fd4eea0704d7d484af828dbc21947d4
Meta Novitia <mnovitia@uci.edu>
2019-01-29 00:53:22 -0800

    Made extra hover for mobile

--- src/components/About/Favorites/Favorites.js
+++ src/components/About/Favorites/Favorites.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Container} from 'reactstrap';
 import './Favorites.css';
 import FlipCard from "./FlipCard/FlipCard";
+import $ from 'jquery';
 
 export default class Favorites extends Component {
 
@@ -9,6 +10,13 @@ export default class Favorites extends Component {
         super(props);
 
         this.favs = props.favs;
+
+        $(document).ready(function() {
+            $('.hover').on('touchstart touchend', function(e) {
+                e.preventDefault();
+                $(this).toggleClass('hover_effect');
+            });
+        });
 
         this.x = window.innerWidth || 
             document.documentElement.clientWidth || 
