0fb79d7c55344450971efe649ca1c5fedb67801d
Meta Novitia <mnovitia@uci.edu>
2019-01-28 21:47:30 -0800

    Updates

---
+++ precache-manifest.6f74359a5d954b6c4880722556b57d5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd2f61002b941a4db01c4b12547ac8b8",
+    "url": "/meta/static/media/Revenant.bd2f6100.otf"
+  },
+  {
+    "revision": "d45540777031f205f936",
+    "url": "/meta/static/css/main.040d84d7.chunk.css"
+  },
+  {
+    "revision": "981e2e0dab32639f191cf3f1c18a4757",
+    "url": "/meta/static/media/Breathe Fire II.981e2e0d.otf"
+  },
+  {
+    "revision": "b42b758b87afd2820b0f",
+    "url": "/meta/static/js/1.b42b758b.chunk.js"
+  },
+  {
+    "revision": "cffb0b4c99caaaca2852",
+    "url": "/meta/static/js/runtime~main.cffb0b4c.js"
+  },
+  {
+    "revision": "278976a6e2fad9d455da1585015c5ae6",
+    "url": "/meta/static/media/Pinch My Ride.278976a6.otf"
+  },
+  {
+    "revision": "d45540777031f205f936",
+    "url": "/meta/static/js/main.d4554077.chunk.js"
+  },
+  {
+    "revision": "83ab11d0894a901b252b571b0a2c16e7",
+    "url": "/meta/static/media/Scared of the Unknown.83ab11d0.otf"
+  },
+  {
+    "revision": "e38683c14c10e79ff9ac74ec6ab0709e",
+    "url": "/meta/static/media/Sunshine.e38683c1.ttf"
+  },
+  {
+    "revision": "b6881b989661e31b42358a6858eb3373",
+    "url": "/meta/static/media/TheHistoriaDemo.b6881b98.ttf"
+  },
+  {
+    "revision": "b42b758b87afd2820b0f",
+    "url": "/meta/static/css/1.54d8ce3f.chunk.css"
+  },
+  {
+    "revision": "a26715e14f13b10f20d3162fd5fd4ca6",
+    "url": "/meta/index.html"
+  }
+];
